d32830bf94b17aa8ebb43ec2688bd762f67afabf
Franco Seguel <165804915+Francoseguel@users.noreply.github.com>
2024-04-17 00:35:36 -0400

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
-
+#Convertidor de farenheits a celsius
 numUno = int(input("Dame la temperatura en farenheit y la convertire en celsius: "))
 celsius = (numUno - 32) * 5 / 9
 print ("Tu temperatura en celsius es de:", celsius)
