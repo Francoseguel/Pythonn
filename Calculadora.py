ed5062b69cd0eb7473b8206d726664bd92199df9
Franco Seguel <165804915+Francoseguel@users.noreply.github.com>
2024-04-19 12:30:29 -0400

    Add files via upload
    brrr calculadora hasta la muerte

---
+++ Calculadora.py
@@ -0,0 +1,23 @@
+a=int(input("Ingrese un numero: "))
+b=int(input("Ingrese un segundo numero: "))
+
+def suma():
+    resultado=a+b
+    print("Su suma es: ", resultado)
+
+def resta():
+    resultado=a-b
+    print("Su resta es: ", resultado)
+
+def multiplicacion():
+    resultado=a*b
+    print("Su multiplicacion es: ", resultado)
+
+def division():
+    resultado=a/b
+    print("Su division es: ", resultado)
+
+suma()
+resta()
+multiplicacion()
+division()
