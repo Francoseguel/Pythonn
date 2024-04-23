45c225f22c264979f52f51a1b31ce012891fb905
Franco Seguel <165804915+Francoseguel@users.noreply.github.com>
2024-04-22 20:08:10 -0400

    Add files via upload

---
+++ ejercicio_etapa_cuantro_act2.2.py
@@ -0,0 +1,24 @@
+print ("Dame tu nombre: ")
+nombr = input()
+nombr_upper = nombr.upper()
+
+print ("Dame tu rut: ")
+
+rut = input()
+rut_upper = rut.upper()
+
+print ("Dame tu correo: ")
+
+correo = input()
+correo_upper = correo.upper()
+
+print ("Dame tu telefono: ")
+
+telefono = input()
+telefono_upper = telefono.upper ()
+
+print ("Sus Datos son: ")
+print ("NOMBRE:\t\t",nombr_upper )
+print("RUT:\t\t", rut_upper)
+print("CORREO:\t\t", correo_upper)
+print("TELEFONO:\t", telefono_upper)
---
+++ ejercicio_etapa_uno_act2.2.py
@@ -0,0 +1,4 @@
+#Ejercicio1
+print ("Bienvenido al mundo de la programacion \nPara comenzar ingresa tu nombre: ")
+nomb = input()
+print("Hola", nomb)
